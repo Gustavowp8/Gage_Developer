34dbf5ff43c83b7ec96d2fce6547cbf06bdc29eb
Gustavo Ferreira dos Santos <gustavoferreira73@live.com>
2022-03-01 22:11:08 -0300

    V1.0.0
    Teste de login

--- estilo/app.js
+++ estilo/app.js
@@ -1,3 +1,23 @@
 function entrar(){
     alert('Em manutenção!')
 }
+
+function fechaEntra(){
+    fer.style.display='none'
+}
+function abirAviso(){
+    fer.style.display='block'
+}
+
+function continuaPro(){
+    location='Login_v1/index.html';
+}
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu);
