91be5d8763a2a9b0c92fbd9a1512179a0ad85ac8
Gustavo Ferreira dos Santos <gustavoferreira73@live.com>
2022-03-17 12:28:47 -0300

    sobre
    pagina sobre agora funciona

---
+++ estilo/SobreApp.js
@@ -0,0 +1,12 @@
+function inicio(){
+    location='index.html'
+   }
+   function planos(){
+     location='planos.html'
+   }
+   function sobre(){
+     location='sobre.html'
+   }
+   function faleConosco(){
+     location='faleConosco.html'
+   }
